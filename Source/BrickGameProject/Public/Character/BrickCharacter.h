6c9575a2cd83a4afd93e4f95435ebfaeb6186591
Yangseongeun <banilla03090@gmail.com>
2025-04-15 17:55:09 +0900

    성은: prev 블럭 수정 & prev 블럭 겹침 수정

--- Source/BrickGameProject/Public/Character/BrickCharacter.h
+++ Source/BrickGameProject/Public/Character/BrickCharacter.h
@@ -13,25 +13,24 @@ struct FInputActionValue;
 UCLASS()
 class BRICKGAMEPROJECT_API ABrickCharacter : public ACharacter
 {
-    GENERATED_BODY()
+	GENERATED_BODY()
 
 public:
-    ABrickCharacter();
+	ABrickCharacter();
 
-	//Components
+	// Components
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
 	class USpringArmComponent* SpringArmComp;
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
 	class UCameraComponent* CameraComp;
 
-	UPROPERTY(EditDefaultsOnly, Category = "Block")
-	TSubclassOf<AActor> PreviewBlockClass;
-	UPROPERTY(EditDefaultsOnly, Category = "Block")
-	TSubclassOf<AActor> PlaceBlockClass;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Block")
+	TArray<TSubclassOf<AActor>> BlockClasses;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Block")
+	TArray<TSubclassOf<AActor>> PreviewBlockClasses;
 
 	float PreviewPivotToBottom = 0.f;
-
-
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "FX")
 	UNiagaraSystem* SpeedFXTemplate;
@@ -39,96 +38,60 @@ public:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "FX")
 	UNiagaraComponent* SpeedFX;
 
-
 protected:
 	virtual void BeginPlay() override;
-
-	bool bCanMove = true; 
+	bool bCanMove = true;
 
 public:
 	virtual void Tick(float DeltaTime) override;
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
-	//Input Functions
-	UFUNCTION()
-	void Move(const FInputActionValue& value);
-	UFUNCTION()
-	void StartJump(const FInputActionValue& value);
-	UFUNCTION()
-	void StopJump(const FInputActionValue& value);
-	UFUNCTION()
-	void Look(const FInputActionValue& value);
-	UFUNCTION()
-	void StartPlacingBlock(const FInputActionValue& value);
-
-	UFUNCTION()
-	void SelectBlock1(const FInputActionValue& value);
-	UFUNCTION()
-	void SelectBlock2(const FInputActionValue& value);
-	UFUNCTION()
-	void SelectBlock3(const FInputActionValue& value);
-
-	UFUNCTION()
-	void RotatePreviewBlock(const FInputActionValue& Value);
-	UFUNCTION()
-	void DeleteBlock(const FInputActionValue& Value);
-	UFUNCTION()
-	void OnLeftClick(const FInputActionValue& Value);
-
-	UFUNCTION()
-	void PlayFKeyAnimationStart(const FInputActionValue& Value);
-
-	UFUNCTION()
-	void PlayFKeyAnimationStop(const FInputActionValue& Value);
+	// Input Functions
+	UFUNCTION() void Move(const FInputActionValue& value);
+	UFUNCTION() void StartJump(const FInputActionValue& value);
+	UFUNCTION() void StopJump(const FInputActionValue& value);
+	UFUNCTION() void Look(const FInputActionValue& value);
+	UFUNCTION() void StartPlacingBlock(const FInputActionValue& value);
+	UFUNCTION() void SelectBlock1(const FInputActionValue& value);
+	UFUNCTION() void SelectBlock2(const FInputActionValue& value);
+	UFUNCTION() void RotatePreviewBlock(const FInputActionValue& Value);
+	UFUNCTION() void DeleteBlock(const FInputActionValue& Value);
+	UFUNCTION() void OnLeftClick(const FInputActionValue& Value);
+	UFUNCTION() void PlayFKeyAnimationStart(const FInputActionValue& Value);
+	UFUNCTION() void PlayFKeyAnimationStop(const FInputActionValue& Value);
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Block")
 	int32 SelectedBlockIndex = 0;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Block")
-	TArray<TSubclassOf<AActor>> BlockClasses;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Animation")
-	UAnimMontage* FKeyMontage;
-
-
 	// 모드 구분용 변수
 	bool bIsPlacingMode;
 
-	// 모드 전환 함수
-
-
 	void AttachCrown();
-
-
 	void PlayVictoryMontage();
-
-
 	void PlayDefeatMontage();
 
 	UPROPERTY(EditDefaultsOnly, Category = "Crown")
 	UStaticMesh* CrownStaticMesh;
 
-
 	UPROPERTY(EditDefaultsOnly, Category = "Animations")
 	UAnimMontage* VictoryMontage;
-
 	UPROPERTY(EditDefaultsOnly, Category = "Animations")
 	UAnimMontage* DefeatMontage;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Animation")
+	UAnimMontage* FKeyMontage;
 
 	// UI 위젯 클래스 (할당용)
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "UI")
 	TSubclassOf<UUserWidget> DamageClass;
 
 	// UI 위젯 인스턴스 (화면에 띄울 용도)
-	UPROPERTY()
-	UUserWidget* DamageInstance;
+	UPROPERTY() UUserWidget* DamageInstance;
 
 	void SetMovementEnabled(bool bEnabled);
-
 	bool CanBeMoved() const;
 
-	AActor* PreviewBlock;
-
+	UPROPERTY() TArray<AActor*> PreviewBlocks;
 	void UpdatePreviewBlock();
 
 	UFUNCTION(NetMulticast, Reliable)
@@ -136,7 +99,6 @@ public:
 
 	UFUNCTION(NetMulticast, Reliable)
 	void MulticastApplyFinalPose(FRotator ActorRot, FRotator MeshRot);
-
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Sound")
 	USoundBase* ClickSound;
@@ -150,23 +112,16 @@ public:
 	UFUNCTION(Server, Reliable)
 	void ServerPlaceBlock(FVector SpawnLocation, FRotator SpawnRotation);
 	void ServerPlaceBlock_Implementation(FVector SpawnLocation, FRotator SpawnRotation);
+
 	UFUNCTION(NetMulticast, Reliable)
 	void MulticastPlayVictoryMontage();
-
 	UFUNCTION(NetMulticast, Reliable)
 	void MulticastPlayDefeatMontage();
 
 	UFUNCTION(BlueprintCallable)
 	void ApplyKnockback(const FVector& Direction, float Strength = 1000.f);
-
 	void EnableMovementAfterKnockback();
 
 	UFUNCTION()
-	void OnHitByObstacle(
-		AActor* SelfActor,
-		AActor* OtherActor,
-		FVector NormalImpulse,
-		const FHitResult& Hit
-	);
-
+	void OnHitByObstacle(AActor* SelfActor, AActor* OtherActor, FVector NormalImpulse, const FHitResult& Hit);
 };
