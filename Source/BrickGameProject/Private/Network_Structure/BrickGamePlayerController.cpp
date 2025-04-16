2ff9754655cfb24406a5ac3f603ff246c84464fd
iiblueblue <58199536+iiblueblue@users.noreply.github.com>
2025-04-16 15:29:09 +0900

    🎉add : Loading UI 생성 🎉add : MapView UI 생성 ✨update : Loading UI 연동 ✨update : MapView UI 연동

--- Source/BrickGameProject/Private/Network_Structure/BrickGamePlayerController.cpp
+++ Source/BrickGameProject/Private/Network_Structure/BrickGamePlayerController.cpp
@@ -17,6 +17,8 @@
 #include "Blueprint/WidgetLayoutLibrary.h"
 #include "InGame/CountdownUserWidget.h"
 #include "Character/BrickCharacter.h"
+#include "InGame/LoadingUserWidget.h"
+#include "InGame/MapViewUserWidget.h"
 
 
 ABrickGamePlayerController::ABrickGamePlayerController()
@@ -42,7 +44,7 @@ ABrickGamePlayerController::ABrickGamePlayerController()
 void ABrickGamePlayerController::BeginPlay()
 {
 	Super::BeginPlay();
-
+	
 	if (HasAuthority())
 	{
 		if (ULocalPlayer* LocalPlayer = GetLocalPlayer())
@@ -60,7 +62,8 @@ void ABrickGamePlayerController::BeginPlay()
 			FString MapName = GetWorld()->GetMapName();
 			if (MapName.Contains("InGameLevel"))
 			{
-				InitInGameUI();
+				UE_LOG(LogTemp, Warning, TEXT("LoadingUIInit"));
+				InitLoadingUI();
 			}
 			else if (MapName.Contains("LobbyLevel"))
 			{
@@ -96,7 +99,8 @@ void ABrickGamePlayerController::PostNetInit()
 		FString MapName = GetWorld()->GetMapName();
 		if (MapName.Contains("InGameLevel"))
 		{
-			InitInGameUI();
+			UE_LOG(LogTemp, Warning, TEXT("LoadingUIInit"));
+			InitLoadingUI();
 		}
 		else if (MapName.Contains("LobbyLevel"))
 		{
@@ -260,6 +264,41 @@ void ABrickGamePlayerController::InitCountdownUI()
 		SetInputMode(InputMode);
 		SetShowMouseCursor(false);
 		FSlateApplication::Get().CancelDragDrop();
+	}
+}
+
+void ABrickGamePlayerController::InitLoadingUI()
+{
+	if (LobbyWidget)
+	{
+		LobbyWidget->RemoveFromParent();
+		LobbyWidget = nullptr;
+	}
+
+	LoadingWidget = CreateWidget<ULoadingUserWidget>(this, LoadingWidgetClass);
+	if (LoadingWidget)
+	{
+		LoadingWidget->AddToViewport();
+		FInputModeUIOnly InputMode;
+		SetInputMode(InputMode);
+		SetShowMouseCursor(false);
+	}
+}
+
+void ABrickGamePlayerController::InitMapViewUI()
+{
+	if (LoadingWidget)
+	{
+		LoadingWidget->RemoveFromParent();
+		LoadingWidget = nullptr;
+	}
+	MapViewWidget = CreateWidget<UMapViewUserWidget>(this, MapViewWidgetClass);
+	if (MapViewWidget)
+	{
+		MapViewWidget->AddToViewport();
+		FInputModeUIOnly InputMode;
+		SetInputMode(InputMode);
+		SetShowMouseCursor(false);
 	}
 }
 
---
+++ Source/BrickGameProject/Public/InGame/MapViewUserWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MapViewUserWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BRICKGAMEPROJECT_API UMapViewUserWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
